c125347f3fdf15a1f9af90ac1a283c654f7ea858
PedroRosaCode <pedrorosa.code@gmail.com>
2025-06-10 19:33:17 -0300

    Fazendo os campos de textos

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,13 @@
+import './CampoTexto.css'
+
+const CampoTexto = () => {
+    return (
+        <div className="campo-texto">
+            <label></label>
+            <input placeholder='Digite o seu nome'></input>
+        </div>
+    )
+
+}
+
+export default CampoTexto
